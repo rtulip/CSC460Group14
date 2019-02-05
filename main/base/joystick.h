a382c9ae4ec4a31aa68722d43aec15958dd80440
Robbie <robert.tulip97@gmail.com>
2019-02-04 19:21:19 -0800

    Updated packet sent & removed pan_and_tilt from base

--- main/base/joystick.h
+++ main/base/joystick.h
@@ -1,30 +1,39 @@
 #include "Arduino.h"
 #define JS_L_X A8
 #define JS_L_Y A9
-#define JS_L_B  52
+#define JS_L_B 52
 
 #define JS_R_X A10
 #define JS_R_Y A11
 #define JS_R_B  53
 
-#define FAST_TURN 10
-#define SLOW_TURN 1
+#define FAST_TURN 0b11
+#define MED_TURN  0b10
+#define SLOW_TURN 0b01
+#define NO_TURN   0b00
+#define BACKWARD  0b000
+#define FORWARD   0b100
 
 #define alpha 0.25
 
-#define LOW_BOUND 200
-#define DEAD_ZONE_LOW 400
+#define LOW_BOUND      200
+#define MED_LOW_BOUND  300
+#define DEAD_ZONE_LOW  400
 #define DEAD_ZONE_HIGH 600
-#define HIGH_BOUND 800
+#define MED_HIGH_BOUND 700
+#define HIGH_BOUND     800
 
-struct return_tuple {
-  int delta_x_L;
-  int delta_y_L;
-  int delta_x_R;
-  int delta_y_R;
+#define LEFT_JS_ID  0x00
+#define RIGHT_JS_ID 0x80
+
+struct returnTuple {
+  char turn_x_L;
+  char turn_y_L;
+  char turn_x_R;
+  char turn_y_R;
 };
 
-void setup_joysticks();
-int calculate_delta(int data);
-struct return_tuple get_increment_values();
-int get_button_data(int right);
+void setupJoysticks();
+char getTurnRate(int data);
+struct returnTuple getTurnValues();
+char getButtonData(int right);
--- main/base/pan_and_tilt.h
+++
@@ -1,6 +0,0 @@
-#define SERVO_PAN   7
-#define SERVO_TILT  6 
-#define MIN_SERVO_TIME 1000
-#define MAX_SERVO_TIME 2000
-
-void increment_servo_value(int* write_time, int delta_value);
