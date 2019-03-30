58da3435f42d172576ee68f1ab6cc5eba0cf345d
BenDHillier <ben@divdot.com>
2019-03-30 02:38:58 -0700

    added comment for analog_read

--- analog/analog.c
+++ analog/analog.c
@@ -7,6 +7,12 @@
 #include <avr/io.h>
 #include "analog.h"
 
+
+/*
+ * pin number corresponds to the Arduino analog pins.
+ * Works for pins 0-7 only.
+ * Pin needs to be set as input (set corresponding DDR bit to 0).
+ */
 int analog_read(int pin) {
 	ADCSRA = 0b10000111;
 	ADMUX = 0b01100000 | (0b00011111 & pin);
--- analog/analog.h
+++ analog/analog.h
@@ -8,6 +8,11 @@
 #ifndef ANALOG_H_
 #define ANALOG_H_
 
+/*
+ * pin number corresponds to the Arduino analog pins.
+ * Works for pins 0-7 only.
+ * Pin needs to be set as input (set corresponding DDR bit to 0).
+ */
 int analog_read(int pin);
 
 #endif /* ANALOG_H_ */
